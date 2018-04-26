184803f57ebfa97856650a845e7c5e66cbc8dc23
Brad <brad@brad.tw>
2018-04-26 10:11:50 +0800

    20180426 v2

--- brad07.php
+++ brad07.php
@@ -1,21 +1,30 @@
 <?php
     $result = $x = $y = '';   // string
-    if (isset($_GET['x']) && isset($_GET['y'])) {
+    if (isset($_GET['x'])) {
         $x = $_GET['x'];
         $y = $_GET['y'];
+        $op = $_GET['op'];
 
-        $result = $x + $y;  // integer
-        //echo "{$x} + {$y} = {$result}";
+        if ($op == '1'){
+            $result = $x + $y;  // integer
+        }else if ($op == '2'){
+            $result = $x - $y;  // integer
+        }else if ($op == '3'){
+            $result = $x * $y;  // integer
+        }else if ($op == '4'){
+            $result = $x / $y;  // integer
+        }
+
 
     }
 ?>
 <form>
     <input type="text" name="x" value="<?php echo $x;  ?>"/>
-    <select>
-        <option>+</option>
-        <option>-</option>
-        <option>x</option>
-        <option>/</option>
+    <select name="op">
+        <option value="1">+</option>
+        <option value="2">-</option>
+        <option value="3">x</option>
+        <option value="4">/</option>
     </select>
     <input type="text" name="y" value="<?php echo $y; ?>" />
     <input type="submit" value="=" />
