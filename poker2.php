bc5e2de7b8d413586423af20f12b575a4d7372ad
Brad <brad@brad.tw>
2018-04-27 16:03:13 +0800

    20180427 v10

--- poker2.php
+++ poker2.php
@@ -17,10 +17,13 @@
 
 <table border="1" width="100%">
     <?php
+        $suits = array('&spades;','&hearts;','&diams;','&clubs;');
+        $v = array('A',2,3,4,5,6,7,8,9,10,'J','Q','K');
         foreach ($players as $player){
+            sort($player);
             echo '<tr>';
             foreach ($player as $p){
-                echo "<td>{$p}</td>";
+                echo "<td>{$suits[(int)($p/13)]}{$v[$p%13]}</td>";
             }
             echo '</tr>';
 
