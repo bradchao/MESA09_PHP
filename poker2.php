027fde540bbd501ba12d30da6ba11f96741e2943
Brad <brad@brad.tw>
2018-04-27 15:31:32 +0800

    20180427 v8

---
+++ poker2.php
@@ -0,0 +1,18 @@
+<?php
+    $poker = range(0,51);
+    shuffle($poker);
+
+    foreach ($poker as $k => $v){
+        echo "{$k} : {$v}<br>";
+    }
+
+    echo '<hr>';
+    $players = array(array(),array(),array(),array(),);
+
+    foreach ($poker as $i => $p){
+        $players[$i%4][] = $p;
+    }
+
+    foreach ($players[3] as $v){
+        echo "{$v}<br>";
+    }
