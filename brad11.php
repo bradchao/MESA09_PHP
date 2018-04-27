717ffeaa36b15a6d86904378d0c0819e97147131
Brad <brad@brad.tw>
2018-04-27 09:46:45 +0800

    20180427 v1

--- brad11.php
+++ brad11.php
@@ -15,4 +15,11 @@
         echo 'E';
     }
 
+    echo '<hr>';
+    $var1 = 10; $var2 = 3;
+    if ($var1-- <= 10 || $var2++ > 3){
+        echo "OK: {$var1} : {$var2}<br>";
+    }else{
+        echo "XX: {$var1} : {$var2}<br>";
+    }
 
---
+++ brad13.php
@@ -0,0 +1,20 @@
+<?php
+
+    $v = 10;
+
+    switch ($v){
+        case 1:
+            echo 'A';
+            break;
+        case 10:
+            echo 'B';
+            break;
+        case 100:
+            echo 'C';
+            break;
+        default:
+            echo 'X';
+            break;
+    }
+
+    echo '<hr>';
