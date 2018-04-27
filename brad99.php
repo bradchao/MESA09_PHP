db124a66ddbc20ad74e8747f9889c15ae3cc979f
Brad <brad@brad.tw>
2018-04-27 11:36:12 +0800

    20180427 v5

--- brad99.php
+++ brad99.php
@@ -1,13 +1,22 @@
 
 <table border="1" width="100%">
     <?php
-        for ($k = 0; $k < 2; $k++ ){
+        for ($k = 0; $k < 4; $k++ ){
             echo '<tr>';
             for ($j = 2; $j <= 5; $j++){
-                echo '<td>';
+                $newj = $j + $k * 4;
+
+//                if (($newj + $k) % 2 == 0){
+//                    echo '<td bgcolor="yellow">';
+//                }else{
+//                    echo '<td bgcolor="pink">';
+//                }
+
+                echo '<td bgcolor="' . ((($newj + $k) % 2 == 0)?'yellow':'pink') . '">';
+
                 for ($i = 1; $i<=9; $i++){
-                    $r = $j * $i;
-                    echo "{$j} x {$i} = {$r}<br>";
+                    $r = $newj * $i;
+                    echo "{$newj} x {$i} = {$r}<br>";
                 }
                 echo '</td>';
             }
