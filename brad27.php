43c4a58c65aedf403388e3803b5c9e4037af5241
Brad <brad@brad.tw>
2018-04-30 16:30:45 +0800

    20180430 v4

--- brad27.php
+++ brad27.php
@@ -1,7 +1,7 @@
 <?php
     include 'bradapis.php';
 
-    $result = '';
+    $result = $hist = '';
     if (!isset($_POST['answer'])){
         // 首次進入
         $answer = createAnswer(3);
@@ -9,7 +9,9 @@
         // 之後進入
         $answer = $_POST['answer'];
         $guess = $_POST['guess'];
+        $hist = $_POST['hist'];
         $result = checkAB($answer, $guess);
+        $hist .= "{$guess} : {$result} <br>";
     }
 
     //echo $answer . '<br>';
@@ -19,6 +21,7 @@
     Guess Number: <input type="number" name="guess"/>
     <input type="submit" value="Guess" />
     <input type="hidden" name="answer" value="<?php echo $answer; ?>">
+    <input type="hidden" name="hist" value="<?php echo $hist; ?>">
 </form>
 <hr />
-<div><?php echo $result; ?></div>
+<div><?php echo $hist; ?></div>
