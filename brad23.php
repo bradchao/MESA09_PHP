9e21a24efb60234219648ef23a17565d85a4bdf4
Brad <brad@brad.tw>
2018-04-30 09:59:51 +0800

    20180430 v1

---
+++ brad23.php
@@ -0,0 +1,12 @@
+<?php
+    sayYa(); sayYa();sayYa();
+    sayHello('Brad');
+
+    function sayYa(){
+        echo 'Ya<br>';
+    }
+
+    function sayHello($name){
+        echo "Hello, {$name}<br>";
+    }
+
