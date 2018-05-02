48e62e4ac1cd2e0acee843178ae1c1450619656b
Brad <brad@brad.tw>
2018-05-02 10:09:26 +0800

    20180502 v1

--- brad42.php
+++ brad42.php
@@ -1,6 +1,7 @@
 <?php
     if (!isset($_FILES['upload'])) die("Server Busy");
 
+    echo 'OK';
     $upload = $_FILES['upload'];
 //    foreach ($upload as $k => $v){
 //        echo "{$k} : {$v}<br>";
