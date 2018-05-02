577d71edff33204efc7276f8e3aa6162afe1de47
Brad <brad@brad.tw>
2018-05-02 12:04:02 +0800

    20180502 v3

--- brad45.php
+++ brad45.php
@@ -1,4 +1,6 @@
 <?php
+    $rate = $_GET['rate']; // 50%
+
     // 產生畫布
     $img = ImageCreateTrueColor(400, 40);
 
@@ -6,7 +8,7 @@
     $yellow = ImageColorAllocate($img,255,255,0 );
     $red = ImageColorAllocate($img,255,0,0 );
     ImageFill ( $img, 0, 0, $yellow);
-    ImageFilledRectangle($img, 0,0,200,40, $red);
+    ImageFilledRectangle($img, 0,0,400*$rate/100,40, $red);
 
     // 輸出
     header('Content-Type: image/jpeg');
