82f231c272d6e59faeb1a5d47479c826060ff4dc
Brad <brad@brad.tw>
2018-05-07 16:30:37 +0800

    20180507 v3

--- editProduct.php
+++ editProduct.php
@@ -9,13 +9,28 @@
         $sql = "select * from product where id={$editid}";
         $result = $mysqli->query($sql);
         $product = $result->fetch_object("Product");
+    }else if (isset($_REQUEST['id'])){
+        $id = $_REQUEST['id'];
+        $pname = $_REQUEST['pname'];
+        $price = $_REQUEST['price'];
+        $qty = $_REQUEST['qty'];
+        $sql = "update product set pname='{$pname}', " .
+            "price={$price}, qty={$qty} " .
+            "where id={$id}";
+        $mysqli->query($sql);
+        header("Location: bmain.php");
 
+    }else{
+        header('Location: bmain.php');
     }
+
+    // update product set field=value, f2=v where id = ?
 
 ?>
 Edit Product:
 <hr />
 <form>
+    <input type="hidden" name="id" value="<?php echo $product->id;?>">
     PName: <input name="pname" value="<?php echo $product->pname;?>"/><br/>
     Price: <input type="number" name="price" value="<?php echo $product->price;?>"/><br/>
     Qty.: <input type="number" name="qty" value="<?php echo $product->qty;?>" /><br/>
