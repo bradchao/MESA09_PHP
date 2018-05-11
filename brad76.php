d4a30d26c346d4e977a91bb9c8807721e9835188
Brad <brad@brad.tw>
2018-05-11 10:37:03 +0800

    20180511 v2

--- brad76.php
+++ brad76.php
@@ -7,5 +7,32 @@
         echo "ERROR: {$error}<br>";
     }
 
+    var_dump($xml);
+    echo '<hr>';
+    echo $xml->getName() . ":" . $xml->count();
+    echo '<hr>';
+    foreach ($xml as $v){
+        echo gettype($v) . ":" . $v->getName() . '<br>';
+    }
+    echo '<hr>';
+    $attrs = $xml->book[0]->attributes();
+    foreach ($attrs as $k => $v){
+        echo "{$k} : {$v}<br>";
+    }
+    echo '<hr>';
+    $cs = $xml->book[0]->children();
+    foreach ($cs as $k => $v){
+        echo "{$k} : {$v}<br>";
+    }
+    echo '<hr>';
+    $as = $xml->book[0]->authors->author;
+    foreach ($as as $k=>$v){
+        echo "{$k} : {$v}<br>";
+    }
+
+
+
+
+
 
 
