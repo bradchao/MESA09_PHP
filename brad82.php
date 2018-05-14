50577d7dc21b554f4af5ecd53b1ffbfec1e3f7c4
Brad <brad@brad.tw>
2018-05-14 15:35:30 +0800

    20180514 v2

---
+++ brad82.php
@@ -0,0 +1,19 @@
+<script>
+    function test1(){
+       var obj = JSON.parse('{"x":"10","y":"3"}');
+        alert(obj.x);
+    }
+</script>
+
+<input type="button" onclick="test1()" value="test1"/>
+
+<div id="container">
+
+</div>
+
+<?php
+
+mail("brad@brad.tw", "title", "OK");
+
+
+
